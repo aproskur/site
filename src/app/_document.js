5410cd2061ec3704a1424d8e56e7c9145d5d5193
aproskur <ao.panova@gmail.com>
2024-03-04 12:47:30 +0000

    t3

--- src/app/_document.js
+++ src/app/_document.js
@@ -1,20 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import Document from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
-        console.log("I AM SERVER PART! BZYK!");
-        const sheet = new ServerStyleSheet();
-        const originalRenderPage = ctx.renderPage;
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
 
         try {
             ctx.renderPage = () =>
                 originalRenderPage({
                     enhanceApp: (App) => (props) =>
                         sheet.collectStyles(<App {...props} />),
-                });
+                })
 
-            const initialProps = await Document.getInitialProps(ctx);
+            const initialProps = await Document.getInitialProps(ctx)
             return {
                 ...initialProps,
                 styles: (
@@ -23,24 +22,9 @@ export default class MyDocument extends Document {
                         {sheet.getStyleElement()}
                     </>
                 ),
-            };
-        } catch (error) {
-            console.error('Error in _document.js getInitialProps:', error);
-            throw error;
+            }
         } finally {
-            sheet.seal();
+            sheet.seal()
         }
-    }
-
-    render() {
-        return (
-            <Html>
-                <Head />
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
     }
 }

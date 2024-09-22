d6d67aa77ffccfddb09a10852f96328834c7b021
aproskur <ao.panova@gmail.com>
2024-09-22 22:53:27 +0100

    improved CSSLoader, removed unnecassary imports:devicons,fontawesome

--- src/app/components/CSSLoader.js
+++ src/app/components/CSSLoader.js
@@ -8,12 +8,24 @@ export default function CSSLoader() {
             const link = document.createElement('link');
             link.rel = 'stylesheet';
             link.href = href;
+
+            link.onload = () => console.log(`Successfully loaded ${href}`);
+            link.onerror = () => console.error(`Failed to load ${href}`);
+
             document.head.appendChild(link);
+            return link; // Return the link element for cleanup
         };
 
-        loadCSS('https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css');
-        loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css');
+        const links = [
+            loadCSS('https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css'),
+            loadCSS('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css'),
+        ];
+
+        return () => {
+            links.forEach(link => {
+                document.head.removeChild(link); // Remove the link when component unmounts
+            });
+        };
     }, []);
 
-    return null; // This component doesn't render anything itself
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -82,10 +82,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${poppinsSemibold.variable} ${poppinsRegular.variable} ${rajdhani.variable} ${wordGameFont.variable} ${archivoNarrow.variable} ${inter.variable} ${styledText.variable}`}>
       <head>
-        <noscript>
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css" />
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
-        </noscript>
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':

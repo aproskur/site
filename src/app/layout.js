559dca09c00aa60fec1c886714b4d7b8c1c0a7e5
aproskur <ao.panova@gmail.com>
2024-12-10 18:40:40 +0000

    preload fonts removed, block instead of swap

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,20 +38,20 @@ const archivoNarrowFont = localFont({
       style: 'normal'
     }
   ],
-  display: 'swap',
+  display: 'block',
   variable: '--font-archivo-narrow'
 });
 
 
 const wordGameFont = localFont({
   src: './assets/fonts/MouseMemoirs-Regular.ttf',
-  display: 'swap',
+  display: 'block',
   variable: '--font-wordGameFont'
 });
 
 const styledText = localFont({
   src: './assets/fonts/Rajdhani/Rajdhani-Regular.ttf',
-  display: 'swap',
+  display: 'block',
   variable: '--font-rajdhani'
 });
 
@@ -76,7 +76,7 @@ const poppinsFont = localFont({
       style: 'normal'
     }
   ],
-  display: 'swap',
+  display: 'block',
   variable: '--font-poppins'
 });
 
@@ -91,27 +91,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${archivoNarrowFont.variable} ${poppinsFont.variable}  ${wordGameFont.variable} ${styledText.variable}`}>
       <head>
-        <link
-          rel="preload"
-          href="/assets/fonts/archivo-narrow/static/ArchivoNarrow-Regular.ttf"
-          as="font"
-          type="font/ttf"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/assets/fonts/Poppins/Poppins-Regular.ttf"
-          as="font"
-          type="font/ttf"
-          crossOrigin="anonymous"
-        />
-        <link
-          rel="preload"
-          href="/assets/fonts/Rajdhani/Rajdhani-Regular.ttf"
-          as="font"
-          type="font/ttf"
-          crossOrigin="anonymous"
-        />
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':

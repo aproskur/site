8cbbbac127e47cdf134d2ff4f5f8958e69705926
aproskur <ao.panova@gmail.com>
2025-02-05 14:36:42 +0000

    f

--- src/i18n/request.js
+++ src/i18n/request.js
@@ -1,11 +1,9 @@
 import { getRequestConfig } from 'next-intl/server';
 import { routing } from './routing';
 
-module.exports = getRequestConfig(async ({ requestLocale }) => {
-    // This typically corresponds to the `[locale]` segment
+export default getRequestConfig(async ({ requestLocale }) => {
     let locale = await requestLocale;
 
-    // Ensure that a valid locale is used
     if (!locale || !routing.locales.includes(locale)) {
         locale = routing.defaultLocale;
     }
--- src/i18n/routing.js
+++ src/i18n/routing.js
@@ -1,24 +1,10 @@
 import { defineRouting } from 'next-intl/routing';
 import { createNavigation } from 'next-intl/navigation';
 
-const routing = defineRouting({
-    // A list of all locales that are supported
+export const routing = defineRouting({
     locales: ['en', 'ru'],
-
-    // Used when no locale matches
     defaultLocale: 'en'
 });
 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
-const { Link, redirect, usePathname, useRouter, getPathname } =
+export const { Link, redirect, usePathname, useRouter, getPathname } =
     createNavigation(routing);
-
-module.exports = {
-    routing,
-    Link,
-    redirect,
-    usePathname,
-    useRouter,
-    getPathname
-};

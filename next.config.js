08b58e57bb1d3d373113502bfbd057cc6b2362a9
aproskur <ao.panova@gmail.com>
2025-02-05 14:15:08 +0000

    another esm module error fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     compiler: {
@@ -9,4 +5,11 @@ const nextConfig = {
     },
 };
 
-export default withNextIntl(nextConfig);
+
+async function loadConfig() {
+    const { default: createNextIntlPlugin } = await import("next-intl/plugin");
+    const withNextIntl = createNextIntlPlugin();
+    return withNextIntl(nextConfig);
+}
+
+module.exports = loadConfig();
